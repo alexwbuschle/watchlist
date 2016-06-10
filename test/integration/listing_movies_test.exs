b73fd5b54a6a3861a3f251e6da267cc493223172
Alex Buschle <alexwbuschle@gmail.com>
2016-06-10 11:32:46 -0300

    Creating movie model + tests

--- test/integration/listing_movies_test.exs
+++ test/integration/listing_movies_test.exs
@@ -2,12 +2,18 @@ defmodule ListingMoviesIntegrationTest do
   use ExUnit.Case, async: true
   use Plug.Test
   alias Watchlist.Router
+  alias Watchlist.Movie
+  alias Watchlist.Repo
 
   @opts Router.init([])
   test 'listing movies' do
+    %Movie{name: "Lord of the Rings", rating: 5} |> Repo.insert!
+    movies = Repo.all(Movie)
+            |> Poison.encode!
+
     conn = conn(:get, "/movies")
     response = Router.call(conn, @opts)
     assert response.status == 200
-    assert response.resp_body == "[]"
+    assert response.resp_body == movies
   end
 end
